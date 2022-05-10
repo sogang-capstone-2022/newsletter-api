517fc6ba32b51c1a023250d9c8d44719a7605880
SEONGILKIM <shiroed1211@naver.com>
2022-05-10 20:59:31 +0900

    Initial commit

---
+++ src/test/java/com/capstone/newsletterapi/NewsletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.newsletterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
